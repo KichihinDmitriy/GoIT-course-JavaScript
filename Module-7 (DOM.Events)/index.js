3030ea8e0504322d763f730d2b02a23c952dd8f5
Dmitriy Kichihin <kichihin.dmitriy1@gmail.com>
2022-04-10 22:34:19 +0300

    aded first task of the module

---
+++ Module-7 (DOM.Events)/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// task 1
+
+const menuItems = document.querySelectorAll('.item');
+const numberOfCategories = menuItems.length;
+
+console.log(`In the list ${numberOfCategories} categories.`);
