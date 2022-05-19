fde81eb6953b0fcfcd25d8e8152a5ac86555e5eb
Dmitriy Kichihin <kichihin.dmitriy1@gmail.com>
2022-05-19 23:43:29 +0300

    completed two tasks using promise

--- Module-11 (Timers. Promise API)/src/index.js
+++ Module-11 (Timers. Promise API)/src/index.js
@@ -1,4 +1,7 @@
-import './switcher';
-import './promisification';
+// import './switcher';
+// import './promisification1';
+// import './promisification2';
+// import './promisification3';
+import './timer';
 
 import './styles.css';
---
+++ Module-11 (Timers. Promise API)/src/timer.js
@@ -0,0 +1,16 @@
+// Countdown timer
+
+// const date = new Date();
+
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
