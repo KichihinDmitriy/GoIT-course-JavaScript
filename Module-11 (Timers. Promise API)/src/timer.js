5f802f2ef6e1ec1e870e96e8dfa579d77a91b8e7
Dmitriy Kichihin <kichihin.dmitriy1@gmail.com>
2022-05-20 16:31:39 +0300

    Created a custom countdown timer plugin

--- Module-11 (Timers. Promise API)/src/timer.js
+++ Module-11 (Timers. Promise API)/src/timer.js
@@ -1,16 +1,38 @@
 // Countdown timer
 
-// const date = new Date();
+const CountdownTimer = function ({selector, targetDate}) {
+    setInterval(() => {
+    const CurrentDate = new Date();
+    const time = targetDate - CurrentDate;
 
-// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const zero = function(value) {
+        return value < 10 ? `0${value}` : value;
+    };
 
-// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const days = zero(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = zero(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = zero(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = zero(Math.floor((time % (1000 * 60)) / 1000));
 
-// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const timerId = document.querySelector(selector);
 
-// const secs = Math.floor((time % (1000 * 60)) / 1000);
+    if(time < 0) {
+        timerId.innerText = 'EXPIRED DATE';
+    } else {
+    timerId.querySelector('span[data-value="days"]').innerText = days;
+    timerId.querySelector('span[data-value="hours"]').innerText = hours;
+    timerId.querySelector('span[data-value="mins"]').innerText = mins;
+    timerId.querySelector('span[data-value="secs"]').innerText = secs;
+    }
+    }, 1000);
+};
 
-// console.log(date.getDate());
-// console.log(date.getHours());
-// console.log(date.getMinutes());
-// console.log(date.getSeconds());
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('May 28, 2022'),
+});
+
+// new CountdownTimer({
+//     selector: '#timer-2',
+//     targetDate: new Date('May 23, 2022'),
+// });
