e5c0d11a4fc56bff0395fb25957df4f905e563ed
Dmitriy Kichihin <kichihin.dmitriy1@gmail.com>
2022-05-22 13:30:47 +0300

    created new 12 module and started to create a small application for searching data about a country by its partial or full name

---
+++ Module-12 (AJAX. Fetch API. CRUD)/README.md
@@ -0,0 +1,1 @@
+# Module-12 (AJAX. Fetch API. CRUD)
---
+++ Module-12 (AJAX. Fetch API. CRUD)/src/index.js
@@ -0,0 +1,40 @@
+import './styles.css';
+const debounce = require('lodash.debounce');
+import itemSample from './item.hbs';
+
+
+const inputJs = document.querySelector('.input-js');
+
+const debouceFn = function (e) {
+    const name = e.target.value;
+
+    const logSuccess = (data) => {
+        console.log(data);
+
+        const listCountries = document.querySelector('.js-countries');
+
+        function buildListCountries (items) {
+            const markup = items.map(item => itemSample(item)).join('');
+
+            console.log(markup);
+        
+            listCountries.insertAdjacentHTML('beforeend', markup);
+        }
+
+        buildListCountries(data);
+    };
+    
+    const logError = err => {
+        console.warn(err);
+    };
+    
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+        if(response.ok) return response.json();
+        throw new Error('Error ' + response.statusText);
+    })
+    .then(logSuccess)
+    .catch(logError)
+}
+
+inputJs.addEventListener('input', debounce(debouceFn, 500));
