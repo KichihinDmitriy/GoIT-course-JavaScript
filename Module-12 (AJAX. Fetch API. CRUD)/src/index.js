38688f2e30f6944989c6b86a6abac8107267b046
Dmitriy Kichihin <kichihin.dmitriy1@gmail.com>
2022-05-25 23:39:26 +0300

    Create a country data lookup application by its partial or full name.

--- Module-12 (AJAX. Fetch API. CRUD)/src/index.js
+++ Module-12 (AJAX. Fetch API. CRUD)/src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 const debounce = require('lodash.debounce');
 import itemSample from './item.hbs';
+import generalSample from './general.hbs';
 
 
 const inputJs = document.querySelector('.input-js');
@@ -12,12 +13,28 @@ const debouceFn = function (e) {
 
         const listCountries = document.querySelector('.js-countries');
 
+        if (listCountries.hasChildNodes()) {
+            listCountries.innerHTML = '';
+        }
+
         function buildListCountries(items) {
             const markup = items.map(item => itemSample(item)).join('');
             listCountries.insertAdjacentHTML('beforeend', markup);
         }
 
-        buildListCountries(data);
+        function buildListCountriesGeneral (item) {
+            const markup = item.map(item => generalSample(item)).join('');
+            listCountries.insertAdjacentHTML('beforeend', markup);
+        }
+
+        if (data.length > 10) {
+            buildListCountries(data);
+            alert('Too many matches found. Please enter a more specific query!');
+        } else if (data.length > 1) {
+            buildListCountries(data);
+        } else if (data.length = 1) {
+            buildListCountriesGeneral(data);
+        }
     };
 
     const logError = err => {
